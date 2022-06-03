79984bf595c12364e1dac6dcc686d9876cae2bef
yonada1k108 <yonamine-daiki-2214@occ.co.jp>
2022-06-03 17:08:30 +0900

    5回目

---
+++ ThanksCardClient/Models/Midle.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanksCardClient.Services;
+
+namespace ThanksCardClient.Models
+{
+    public class Midle : BindableBase
+    {
+        #region 
+    }
+    #endregion
+}
--- ThanksCardClient/Models/Template.cs
+++ ThanksCardClient/Models/Template.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿#nullable disable
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThanksCardClient.Services;
+
 
 namespace ThanksCardClient.Models
 {
-    internal class Template
+    internal class Template : BindableBase
     {
-        #region
+        #region IdProperty
+        private long _Id;
+        public long Id
+        {
+            get { return _Id; }
+            set { SetProperty(ref _Id, value); }
+        }
+        #endregion
+
+        #region TemplateNameProperty
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { SetProperty(ref _Name, value); }
+        }
+        #endregion
+
+        #region TemplateContentProperty
+        private string _Content;
+        public string Content
+        {
+            get { return _Content; }
+            set { SetProperty(ref _Content, value); }
+        }
+        #endregion
     }
 }
